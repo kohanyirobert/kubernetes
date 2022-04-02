1fba56959ad7b96328bb802a0dd104ee3097fdad
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-04-02 11:49:20 +0200

    cfssl installation script

---
+++ scripts/cfssl.sh
@@ -0,0 +1,15 @@
+# Installs Cloudflare's cfssl toolkit for working with certificates
+cfssl_version=1.6.1
+cfssl_os=linux
+cfssl_platform=amd64
+cfssl_executable=cfssl_${cfssl_version}_${cfssl_os}_${cfssl_platform}
+cfssljson_executable=cfssljson_${cfssl_version}_${cfssl_os}_${cfssl_platform}
+cfssl_url_prefix=https://github.com/cloudflare/cfssl/releases/download/v$cfssl_version
+wget --timestamping $cfssl_url_prefix/$cfssl_executable
+wget --timestamping $cfssl_url_prefix/$cfssljson_executable
+sudo install \
+  --verbose \
+  --mode=+x $cfssl_executable /usr/local/bin/cfssl
+sudo install \
+  --verbose \
+  --mode=+x $cfssljson_executable /usr/local/bin/cfssljson
