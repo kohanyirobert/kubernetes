b14182702f73f1c31dcd4c537614590df73fefbc
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-04-15 17:18:07 +0200

    Add docker.local /etc/hosts

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -1,11 +1,15 @@
 # Appends IPs of all nodes on every node to /etc/hosts
+add_host() {
+  pattern=$1
+  entry=$2
+  grep --quiet "$pattern" /etc/hosts \
+    || echo "$entry" \
+    | sudo tee --append /etc/hosts
+}
+
 node_hostname=$1
 node_ip=$2
 
-grep --quiet cluster-endpoint /etc/hosts \
-  || echo "$node_ip cluster-endpoint" \
-  | sudo tee --append /etc/hosts
-
-grep --quiet "$node_ip $node_hostname" /etc/hosts \
-  || echo "$node_ip $node_hostname" \
-  | sudo tee --append /etc/hosts
+add_host cluster-endpoint "$node_ip cluster-endpoint"
+add_host "$node_ip $node_hostname" "$node_ip $node_hostname"
+add_host docker.local "10.0.100.100 docker.local"
