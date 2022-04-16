83ab86b452185596b440c086e6d4bf67ee7f8ea2
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-04-16 13:37:24 +0200

    Update nginx-ingress script with namespace variable

--- scripts/nginx-ingress.sh
+++ scripts/nginx-ingress.sh
@@ -1,8 +1,9 @@
 # Installs NGINX Ingress Controller via Helm
 # https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-helm/
-if ! kubectl wait pod --all --for=condition=Ready --namespace nginx-ingress --timeout=0s
+nginx_ingress_namespace=nginx-ingress
+nginx_ingress_version=2.1.1
+if ! kubectl wait pod --all --for=condition=Ready --namespace $nginx_ingress_namespace --timeout=0s
 then
-  nginx_ingress_version=2.1.1
   if [ ! -d kubernetes-ingress ]
   then
     git clone --branch v$nginx_ingress_version https://github.com/nginxinc/kubernetes-ingress/
@@ -11,14 +12,14 @@ then
   kubectl apply -f crds/
   helm upgrade nginx-ingress . \
     --install \
-    --namespace nginx-ingress \
+    --namespace $nginx_ingress_namespace \
     --create-namespace
   cd -
-  until kubectl wait pod --all --for=condition=Ready --namespace  nginx-ingress
+  until kubectl wait pod --all --for=condition=Ready --namespace $nginx_ingress_namespace
   do
     echo "warning: resources don't exist"
     sleep 1
   done
   echo "info: resources exist"
-  kubectl wait pod --all --for=condition=Ready --namespace  nginx-ingress --timeout=120s
+  kubectl wait pod --all --for=condition=Ready --namespace $nginx_ingress_namespace --timeout=120s
 fi
