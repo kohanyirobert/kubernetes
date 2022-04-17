4759af49c58b4fc87afd9db01c60f168d55071c2
Kohányi Róbert <kohanyi.robert@gmail.com>
2022-04-17 17:32:08 +0200

    Install calicoctl

--- scripts/calico.sh
+++ scripts/calico.sh
@@ -24,3 +24,13 @@ then
   echo "info: resources exist"
   kubectl wait pod --all --for=condition=Ready --namespace calico-system --timeout=120s
 fi
+
+calicoctl_version=${calico_version}.2
+calicoctl_os=linux
+calicoctl_platform=amd64
+calicoctl_binary=calicoctl-$calicoctl_os-$calicoctl_platform
+calicoctl_url=https://github.com/projectcalico/calico/releases/download/v$calicoctl_version/$calicoctl_binary
+wget --no-verbose --timestamping $calicoctl_url
+sudo install \
+  --verbose \
+  --mode=+x $calicoctl_binary /usr/local/bin/calicoctl
